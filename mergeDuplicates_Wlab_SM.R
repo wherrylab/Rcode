76f1d70c75c669c254a1f84cb7a532a13a65a4d5
Sasi <sasikanthmanne@gmail.com>
2017-02-28 11:02:05 -0500

    Added purpose of the function and caveats

--- mergeDuplicates_Wlab_SM.R
+++ mergeDuplicates_Wlab_SM.R
@@ -1,3 +1,4 @@
+#Function to collapse multiple probes to genes and multiple peaks annotated to a single gene,what stat to use to merge the values is debatable and there is no standard
 mergeDuplicates <- function(data, mergeMethod){
   #Function to merge duplicate gene level expression data using one of the mean,median,max methods,the input data should
   # have first column as the gene and rest of the data numeric expression data.
